e2f278983f4c54d46a1891cae7c6f92ab035423e
Pankaj-Mali <malipankaj660@gmail.com>
2024-08-14 18:50:27 +0530

    for deploy

--- server.js
+++ server.js
@@ -24,5 +24,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/items', itemRoutes);
 app.use('/api/orders', orderRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;
